cca5da068226fcda9a1019415f68be3b69c6d07b
saikumarpb <saipusarla7094@gmail.com>
2023-03-24 17:21:21 +0530

    Add ui to sol faucet

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { Connection, PublicKey } from "@solana/web3.js";
 import React, { useState } from "react";
-import { DEV_NET_RPC_URL } from "./constants";
+import { DEV_NET_RPC_URL, TEST_NET_RPC_URL } from './constants';
+import './App.css';
+import Header from './Header';
+import { Button, Form, Modal } from 'react-bootstrap';
+import SolanaLogo from 'jsx:./assets/SolanaLogo.svg';
 
+type RPC = 'devnet' | 'testnet';
 function App() {
-  const [txHash, setTxHash] = useState("");
-  const [publicAddress, setPublicAddress] = useState("");
+  const [txHash, setTxHash] = useState('');
+  const [publicAddress, setPublicAddress] = useState('');
   const [isValidAddress, setIsValidAddress] = useState(true);
+  const [showSuccessModal, setShowsuccessModal] = useState(false);
+  const [rpc, setRpc] = useState<RPC>('devnet');
 
+  /**
+   * Validates the address provided
+   * @param address solana public wallet address
+   */
   const validateSolanaAddress = (address: string) => {
     try {
       let publicKey = new PublicKey(address);
@@ -19,41 +30,112 @@ function App() {
     }
   };
 
-  const requestAirdrop = async (publicKey: string) => {
+  const requestAirdrop = async (publicKey: string, net: RPC) => {
     if (validateSolanaAddress(publicKey)) {
-      const connection = new Connection(DEV_NET_RPC_URL);
+      const connection = new Connection(
+        net === 'devnet' ? DEV_NET_RPC_URL : TEST_NET_RPC_URL
+      );
       await connection
         .requestAirdrop(new PublicKey(publicKey), 1e9)
         .then((data) => {
           setTxHash(data);
+          setShowsuccessModal(true);
         })
         .catch((e) => console.error(e.toString()));
     } else {
     }
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    requestAirdrop(publicAddress);
+  const handleSubmit = (net: RPC) => {
+    setRpc(net);
+    requestAirdrop(publicAddress, rpc);
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPublicAddress(e.target.value);
+    setIsValidAddress(true);
   };
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input value={publicAddress} onChange={handleInputChange} />
-        <button type="submit">submit</button>
-      </form>
-      {isValidAddress && txHash && (
+  const renderSuccessModal = () => (
+    <Modal
+      show={showSuccessModal}
+      onHide={() => setShowsuccessModal(!showSuccessModal)}
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Successful airdrop!
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="d-flex flex-column">
         <div>
-          <span>Tx hash : </span>
-          {txHash}
+          <p>Successfully airdropped 1 SOL to {publicAddress}</p>
         </div>
-      )}
-      {!isValidAddress && <div>Invalid wallet address</div>}
+        <a
+          target="_blank"
+          href={`https://explorer.solana.com/tx/${txHash}?cluster=${rpc}`}
+        >
+          View transaction on solana explorer
+        </a>
+      </Modal.Body>
+      <Modal.Footer>
+        <button
+          onClick={() => setShowsuccessModal(!showSuccessModal)}
+          type="button"
+          className="submit-btn"
+        >
+          Go back to SOL faucet
+        </button>
+      </Modal.Footer>
+    </Modal>
+  );
+
+  return (
+    <div className="app">
+      <div className="d-flex flex-column justify-content-center body">
+        <div className="pb-5">
+          <SolanaLogo height="40px" width="270px" />
+        </div>
+
+        <h2 className="text-white pb-4">SOL Faucet</h2>
+        <h3 className="sub-heading pb-4">Get your airdrop</h3>
+        <Form className="form">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control
+              placeholder="Enter Solana account address"
+              value={publicAddress}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+          {!isValidAddress && (
+            <div className="mb-3 text-light border rounded border-danger text-center">
+              Invalid wallet address
+            </div>
+          )}
+
+          <div className="d-flex justify-content-center gap-4 py-4">
+            <button
+              type="button"
+              className="submit-btn px-2 py-1"
+              onClick={() => handleSubmit('devnet')}
+            >
+              Devnet
+            </button>
+            <button
+              type="button"
+              className="submit-btn px-2 py-1"
+              onClick={() => handleSubmit('testnet')}
+            >
+              Testnet
+            </button>
+          </div>
+          <p className="text-white pb-4 text-center">
+            With the click of a button, fund your SOL testnet or devnet wallet
+          </p>
+        </Form>
+        {renderSuccessModal()}
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-
-const rootContainer = document.getElementById("root");
+import App from './App';
+const rootContainer = document.getElementById('root');
 const root = createRoot(rootContainer!);
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 root.render(<App/>)
+
+// Green : 1ED79A
+// Black : 161B19
+// Grey : 1E2423
+// White : FFFFFF
+// Disabled Green : 698582
